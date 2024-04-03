11684c4f4c1bb841aa73bb95ae4f84272f77666c
Krishna Patel <krishnapatel6380@gmail.com>
2024-04-03 20:16:18 +0530

    lab 5 finished

--- models/eventModel.js
+++ models/eventModel.js
@@ -4,7 +4,7 @@ const eventSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     date: { type: Date, required: true },
-    location: { type: String, required: true  },
+    location: { type: String,required: true  },
   });
 
 module.exports = mongoose.model('Event', eventSchema);
